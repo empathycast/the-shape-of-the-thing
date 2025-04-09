ecbab49507e0aa55a2053be4dee60a77de0c8442
empathycast <empathycast@gmail.com>
2025-04-09 09:03:25 -0700

    component footer on index

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const placeholder = document.getElementById("footer-placeholder");
   if (placeholder) {
-    fetch("../../components/footer.html")
+    fetch("/components/footer.html")
       .then(res => res.text())
       .then(html => {
         placeholder.innerHTML = html;
@@ -11,9 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
         const isDark = localStorage.getItem("dark-mode") === "true";
         if (toggleImg) {
           toggleImg.src = isDark
-            ? "../../images/icons/light.png"
-            : "../../images/icons/dark.png";
+            ? "/images/icons/light.png"
+            : "/images/icons/dark.png";
         }
       });
   }
 });
+
